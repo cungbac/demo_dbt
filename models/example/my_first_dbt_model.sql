177c8991199646d1536f29220ba141b3c0ca57b3
unknown <cungbac.tran@gmail.com>
2021-09-03 13:40:08 +0700

    update

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -10,23 +10,40 @@
 -- Test ephemeral materialized
 -- {{ config(materialized='ephemeral') }}
 
-{{config(materialized='table')}}
+{{config(materialized='table', 
+        alias='first_model')}}
+        -- post_hook='grant select on {{this}} to role analyst')}}
 
-with source_data as (
+-- with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
-    union all
-    select 4 as id
+--     select 1 as id
+--     union all
+--     select null as id
+--     union all
+--     select 4 as id
 
-)
+-- )
 
-select *
-from source_data
 
+-- select *, {{ var('my_first_variable') }} as my_first_variable
+-- from source_data
+-- where id >= {{ var('my_second_variable') }}
 /*
     Uncomment the line below to remove records with null `id` values
 */
 
 -- where id is not null
+
+
+-- Snapshots
+with source_data as 
+(
+    select 1 as id, 'NY' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
+    union all
+    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    union all
+    select 3 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+)
+
+select *
+from source_data
--- models/example/snowflake_customer_purchases.sql
+++ models/example/snowflake_customer_purchases.sql
@@ -3,8 +3,12 @@ select
     c.c_name,
     c.c_nationkey as nation,
     sum(o.o_totalprice) as total_order_price
-from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
-left join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+-- from "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."CUSTOMER" c
+-- left join "SNOWFLAKE_SAMPLE_DATA"."TPCH_SF1"."ORDERS" o
+
+from {{source('sample_data','customer')}} c
+left join {{source('sample_data','orders')}} o
+
 on c.c_custkey = o.o_custkey
 group by 
     c.c_custkey,
